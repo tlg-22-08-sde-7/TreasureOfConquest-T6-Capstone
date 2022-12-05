67602ba2668fe5e4537e020e26110c179a6e0833
dominquelawson <dominque.lawson115@gmail.com>
2022-12-05 15:10:07 -0800

    updated the game controller to give the option for new game and added player class, AB#145

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,8 +1,16 @@
 package controller;
 
+import com.apps.util.Console;
+import com.apps.util.Prompter;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         GameController game = new GameController();
+        game.welcomeScreen();
+        Console.pause(2000L);
+        Console.clear();
         game.run();
     }
 
