bf2e4364fda3ff3673789f21723e56b64114c17f
dominquelawson <dominque.lawson115@gmail.com>
2022-12-08 12:29:23 -0800

    added NPC object model, updated json file to match model. Populates a list of objects from json, AB#161

---
+++ src/model/NPC.java
@@ -0,0 +1,39 @@
+package model;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class NPC {
+    private String npcType;
+    private String [] commands;
+    private String [] responds;
+
+    public NPC (){
+
+    }
+
+    public NPC (String npcType,String [] commands, String [] responds){
+        this.npcType = npcType;
+        this.commands = commands;
+        this.responds = responds;
+    }
+
+    public void populateNPCList(){
+        try {
+            Gson gson = new Gson();
+            Reader reader = Files.newBufferedReader(Path.of("assets/json-files/npcWordBank.json"));
+            List<NPC> npcList = Arrays.asList(gson.fromJson(reader,NPC[].class));
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
